c15876c29c44df995283ec820838f5ef4eaf3296
DineshnarasimmanMSF4057 <125851531+DineshnarasimmanMSF4057@users.noreply.github.com>
2026-09-09 21:04:25 +0530

    868499: updated routes

--- data-grid/react-samples/react-grid-row-column-pinning/src/App.jsx
+++ data-grid/react-samples/react-grid-row-column-pinning/src/App.jsx
@@ -1,19 +1,9 @@
-import { HashRouter as Router, Routes, Route } from 'react-router';
 import './App.css';
-import { getPublicBasePath } from './basePath.ts';
 import DataGrid from './components/Grid';
 
 function App() {
   return (
-    <Router basename={getPublicBasePath()}>
-      <div className="app-container">
-        <main className="app-main">
-          <Routes>
-            <Route path="/" element={<DataGrid />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <DataGrid />
   );
 }
 
